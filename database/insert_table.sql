4b0bb633e280fac03a8d0e0c29558e702b2a9b49
NguyenQuy03 <huyquy2003@gmail.com>
2022-08-30 21:40:15 +0700

    check commit part 2

--- database/insert_table.sql
+++ database/insert_table.sql
@@ -15,6 +15,11 @@ VALUES ("English Vocabulary in Use: Advanced","https://covers.zlibcdn2.com/cover
 "The words you need to communicate with confidence. Vocabulary explanations and practice for advanced level (C1 to C2) learners of English.",
 "Michael McCarthy, Felicity O'Dell", 1, "https://book4you.org/dl/4985683/e44d03");
 
+INSERT INTO book(title, thumbnail, shortDescription, author, categoryId, linkDownload)
+VALUES ("Perfect English Grammar","https://covers.zlibcdn2.com/covers299/books/4a/65/86/4a658614f8ce539ec9a412cbe722983a.jpg",
+"Expert linguist Grant Barrett gives you all the tools you need to improve your everyday communication―from perfecting your punctuation to polishing your speaking skills―with his accessible, go-to grammar guide.",
+"Grant Barrett", 1, "https://book4you.org/dl/2857818/15aa77");
+
 //Comment
 INSERT INTO comment(content, userId, bookId)
 VALUES ("Thanks for sharing so good word book! It is very useful for me.",
